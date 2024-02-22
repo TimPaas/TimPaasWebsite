7d41965d4f42e1b72d8b746d3b6ad221086df365
TimP <83457584+TimPaas@users.noreply.github.com>
2024-02-22 13:07:58 +0100

    test

--- backend/Mailservice.cs
+++ backend/Mailservice.cs
@@ -1,10 +1,20 @@
 using SendGrid.Helpers.Mail;
 using SendGrid;
+using System;
+using System.Web;
+
 try
 {
-    
+    // Retrieve form data from the HTTP POST request
+    var formData = HttpContext.Current.Request.Form;
+    var firstName = formData["firstname"];
+    var lastName = formData["lastname"];
+    var email = formData["email"];
+    var messageSubject = formData["subject"];
+    var messageBody = formData["Message"];
+
+    // Your SendGrid API key
     var apiKey = Environment.GetEnvironmentVariable("tim");
-    Console.WriteLine(apiKey);
 
     if (string.IsNullOrEmpty(apiKey))
     {
@@ -15,23 +25,18 @@ try
     var from = new EmailAddress("timpaaswebsite@gmail.com", "Tim");
     var subject = emailSubject;
 
-    // Extract form data
-    var formData = HttpContext.Current.Request.Form;
-    var firstName = formData["firstname"];
-    var lastName = formData["lastname"];
-    var email = formData["email"];
-    var messageSubject = formData["subject"];
-    var messageBody = formData["Message"];
-
     // Create email content using form data
     var emailMessage = $"Name: {firstName} {lastName}\nEmail: {email}\nSubject: {messageSubject}\nMessage: {messageBody}";
 
     var to = new EmailAddress("timpaaswebsite@gmail.com", "Tim");
     var plainTextContent = emailMessage;
     var htmlContent = emailMessage;
-    var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
 
+    // Send the email
+    var client = new SendGridClient(apiKey);
+    var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
     var response = await client.SendEmailAsync(msg);
+
     Console.WriteLine(response.StatusCode);
 }
 catch (Exception ex)
